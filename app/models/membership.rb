3ff90acefe5ea3c649e9e8d6a9d93bee6bc9fc60
David Busse <dpb@essub.com>
2013-01-03 20:14:02 -0600

    temporarily remove validations for memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,5 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
   
-  validates :user, uniqueness: { scope: :group_id, message: "already in that group" }
+  #validates :user, uniqueness: { scope: :group_id, message: "already in that group" }
 end
