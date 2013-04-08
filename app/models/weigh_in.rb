82feff8c88f3d4e5d3c815e6af058f311efaed82
Nick Pazoles <np7776@gmail.com>
2013-04-07 20:52:01 -0500

    fix bug where no weights between filter would cause crash

--- app/models/weigh_in.rb
+++ app/models/weigh_in.rb
@@ -6,14 +6,6 @@ class WeighIn < ActiveRecord::Base
   validates :date, presence: true
   validates :weight, numericality: true
   validates :date, uniqueness: { scope: :user_id, message: "already has a weight recorded for it" }
-
-  def self.dates_from_group(group_id)
-    WeighIn.joins(:user => :groups).where("groups.id = ?", group_id).order("date ASC").select(:date).uniq
-  end
   
-  def display_date
-    date.to_date.strftime('%B %d %Y')
-  end
-
 end
 
